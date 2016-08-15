d20cfdf92b055fae59b4d746b769355e24f25134
Jonathon Chappell <jc300329@ad.jcu.edu.au>
2016-08-15 14:40:10 +1000

    Fixed all files not using DebugBox class

--- DebuggingExercises/DebugFour4.java
+++ DebuggingExercises/DebugFour4.java
@@ -6,19 +6,19 @@ public class DebugFour4
       final double DISCOUNT_RATE = 0.90;
       int price = 100;
       double price2 = 100.00;
-      tenPercentOff(price, DISCOUNT_RATE);
-      tenPercentOff(price2, DISCOUNT_RATE);
+      tenPercentOffint(price, DISCOUNT_RATE);
+      tenPercentOffdouble(price2, DISCOUNT_RATE);
    }
-   public static void tenPercentOff(int p)
+   public static void tenPercentOffint(int p, double DISCOUNT_RATE)
    {
       double newPrice = p * DISCOUNT_RATE;
-      System.out.println("Ten percent off  + p);
-      System.out.println("  New price is " + newPrice);
-   }
-   public static void tenPercentOff(double p)
-   {
-      double newPrice = DISCOUNT_RATE;
       System.out.println("Ten percent off " + p);
-      System.out.println("  New price is " + newPrice);
+      System.out.println("New price is " + newPrice);
+   }
+   public static void tenPercentOffdouble(double p, double DISCOUNT_RATE)
+   {
+      double newPrice = p * DISCOUNT_RATE;
+      System.out.println("Ten percent off " + p);
+      System.out.println("New price is " + newPrice);
    }
 }
